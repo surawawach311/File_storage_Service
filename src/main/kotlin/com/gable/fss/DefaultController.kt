8afad30c2483761a761d02dbf667b1c73e8ab2f1
Teerapong Suksuwan <trapongx@gmail.com>
2020-07-29 17:18:34 +0700

    Add content type and auto create subfolder

--- src/main/kotlin/com/gable/fss/DefaultController.kt
+++ src/main/kotlin/com/gable/fss/DefaultController.kt
@@ -6,6 +6,8 @@ import org.springframework.core.io.FileSystemResource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.util.MimeType
+import org.springframework.util.MimeTypeUtils
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
@@ -20,6 +22,11 @@ open class DefaultController {
     fun upload(@RequestParam("path") path: String, @RequestParam("file") file: MultipartFile): ResponseEntity<ResponseDetails> {
         return try {
             val dest = File(fssConfig.rootLocation + path)
+            dest.parentFile.also {
+                if (!it.exists()) {
+                    it.mkdirs()
+                }
+            }
             IOUtils.copy(file.inputStream, dest.outputStream())
             ResponseEntity.ok().body(
                 ResponseDetails().also {
@@ -41,7 +48,9 @@ open class DefaultController {
         val src = File(fssConfig.rootLocation + path)
         if (!src.exists())
             return ResponseEntity.notFound().build()
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-            "attachment; filename=\"" + src.name + "\"").body(FileSystemResource(src))
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + src.name + "\"")
+            .header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+            .body(FileSystemResource(src))
     }
 }
